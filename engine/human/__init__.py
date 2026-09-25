418f381652c5814578ce79016cc84c5531a4948f
E. <45076065+eishied@users.noreply.github.com>
2026-09-25 15:13:55 -0600

    phase9: export interplanetary settlement model

--- engine/human/__init__.py
+++ engine/human/__init__.py
@@ -4,5 +4,6 @@ from .settlement import evaluate_settlement_support
 from .planetary_history import simulate_planetary_history
 from .demography import simulate_demography
 from .astroanthropology import simulate_astroanthropology
+from .interplanetary import build_world_catalog, simulate_interplanetary_settlement
 
-__all__ = ["evaluate_settlement_support", "simulate_planetary_history", "simulate_demography", "simulate_astroanthropology"]
+__all__ = ["evaluate_settlement_support", "simulate_planetary_history", "simulate_demography", "simulate_astroanthropology", "build_world_catalog", "simulate_interplanetary_settlement"]
