fde249d2cfbc11cac2ca1c55c9af61c60535a0f2
E. <45076065+eishied@users.noreply.github.com>
2026-09-25 15:31:06 -0600

    phase9.1: export interplanetary network model

--- engine/human/__init__.py
+++ engine/human/__init__.py
@@ -5,5 +5,6 @@ from .planetary_history import simulate_planetary_history
 from .demography import simulate_demography
 from .astroanthropology import simulate_astroanthropology
 from .interplanetary import build_world_catalog, simulate_interplanetary_settlement
+from .interplanetary_network import simulate_interplanetary_network
 
-__all__ = ["evaluate_settlement_support", "simulate_planetary_history", "simulate_demography", "simulate_astroanthropology", "build_world_catalog", "simulate_interplanetary_settlement"]
+__all__ = ["evaluate_settlement_support", "simulate_planetary_history", "simulate_demography", "simulate_astroanthropology", "build_world_catalog", "simulate_interplanetary_settlement", "simulate_interplanetary_network"]
