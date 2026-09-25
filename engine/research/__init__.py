d05e272f204994e726a4932296b92a4753fd3f6b
E. <45076065+eishied@users.noreply.github.com>
2026-09-25 16:31:32 -0600

    phase11.2: export manuscript review gate

--- engine/research/__init__.py
+++ engine/research/__init__.py
@@ -2,5 +2,6 @@
 
 from .release import build_research_release_manifest
 from .note_builder import build_research_note
+from .review_gate import evaluate_manuscript_review_gate
 
-__all__ = ["build_research_release_manifest", "build_research_note"]
+__all__ = ["build_research_release_manifest", "build_research_note", "evaluate_manuscript_review_gate"]
