32313b4f9497ba720e37887caa8908d72e9d57e0
E. <45076065+eishied@users.noreply.github.com>
2026-09-25 13:29:26 -0600

    phase7: export planetary history model

--- engine/human/__init__.py
+++ engine/human/__init__.py
@@ -1,5 +1,6 @@
 """Human settlement-support models for TRISOLARIS LAB."""
 
 from .settlement import evaluate_settlement_support
+from .planetary_history import simulate_planetary_history
 
-__all__ = ["evaluate_settlement_support"]
+__all__ = ["evaluate_settlement_support", "simulate_planetary_history"]
